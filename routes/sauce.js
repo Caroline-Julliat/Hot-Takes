cffeef6de1fe6378809916ec1ce5d11944e8747a
Caroline Julliat <carolinejulliat@gmail.com>
2023-02-14 10:20:36 +0100

    Projet Fonctionnel sans la route POST/like

---
+++ routes/sauce.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const router = express.Router()
+
+const sauceCtrl = require("../controllers/sauce")
+const multer = require("../middleware/multer-config")
+const auth = require("../middleware/auth")
+
+router.post("/", auth, multer, sauceCtrl.createSauce)
+router.put("/:id", auth, multer, sauceCtrl.modifySauce)
+router.delete("/:id", auth, sauceCtrl.deleteSauce)
+router.get("/:id", auth, sauceCtrl.getOneSauce)
+router.get("/", auth, sauceCtrl.getAllSauces)
+// router.post("/:id/like", sauceCtrl.likeSauce)
+
+module.exports = router
